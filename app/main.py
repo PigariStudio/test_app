a7b10615f4a6b17d7f073e7a3e15b2f57b90b542
Pigari Studio <pigari.studio@gmail.com>
2024-09-12 21:28:39 +0200

    Update main.py

--- app/main.py
+++ app/main.py
@@ -3,14 +3,14 @@ import tkinter as tk
 def create_window():
     # Create the main window
     root = tk.Tk()
-    root.title("Simple Tkinter App")
+    root.title("test_app - ")
 
     # Create a label widget
-    label = tk.Label(root, text="Hello, Tkinter!")
+    label = tk.Label(root, text="this is a test_app for PigarOS")
     label.pack(pady=10)
 
     # Create a button widget
-    button = tk.Button(root, text="Click Me", command=lambda: print("Button clicked!"))
+    button = tk.Button(root, text="Exit", command=root.destroy)
     button.pack(pady=10)
 
     # Start the Tkinter event loop
